a4f967e87f6183cf3557c1dab316a6a8d745e530
User <User@DESKTOP-2I553F1>
2023-12-23 03:17:20 +0400

    Добавьте файлы проекта.

---
+++ 21  21  21/eee.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <windows.h>
+void drawMap(HDC hdc);
+void midas();
+void Left();
+void Right();
+void Up();
+void Down();
+void wall();
+void zoloto();
+void Up12();
+void Down12();
